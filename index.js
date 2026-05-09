7d22d11f1e5981007a3efbd614138aab16d02fbb
vjharish7 <vijayharish2398@gmail.com>
2026-05-09 07:02:07 +0000

    Month 1 - async/await lesson

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
-const user = {
-    id: 1,
-    username: "theharmony",
-    email: "gvabeme124ceg@gmail.com",
-    bio: "Building something real",
-    followerCount: 0,
-    isLoggedIn: true
-};
-console.log(user.username);
-console.log(user.email);
-console.log(user.followerCount);
-user.followerCount = 10;
-user.bio = "Building a social platform";
-user.joinedDate = "2026-05-09";
-console.log(user);
-const post = {
-  id: 1,
-  userId: 1,
-  content: "My first post",
-  likes: 0,
-  createdAt: "2026-05-09"
-};
-const notification = {
-  id: 1,
-  userId: 1,
-  type: "like",
-  isRead: false
-};
-const users = [
-  { id: 1, username: "theharmony", followerCount: 10 },
-  { id: 2, username: "vjharish7", followerCount: 25 },
-  { id: 3, username: "buildersclub", followerCount: 5 }
-];
-console.log(users.length);
-console.log(users[0]);
-console.log(users[1].username);
-function findUser(username) {
-  return users.find(user => user.username === username);
+function getUserFromDatabase(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ id: id, username: "theharmony", email: "gvabeme124ceg@gmail.com" });
+    }, 1000); // waits 1 second to simulate database delay
+  });
 }
-function addUser(id, username, followerCount) {
-  const newUser = { id, username, followerCount };
-  users.push(newUser);
-  console.log("User added:", newUser);
+function getBrokenUser() {
+  const user = getUserFromDatabase(1); // doesn't wait
+  console.log("Got user:", user);      // prints before user arrives ❌
 }
-console.log(findUser("vjharish7"));
-addUser(4, "newmember", 0);
-console.log(users.length);
+
+getBrokenUser();
+async function getCorrectUser() {
+  const user = await getUserFromDatabase(1);
+  console.log("Got user:", user);
+}
+
+getCorrectUser();
